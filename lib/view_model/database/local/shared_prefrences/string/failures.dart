ad5acc00bf86a0e2ce0f35a59f9d3246f204bc16
Amira Zahran <67193358+Amira-Zahran@users.noreply.github.com>
2022-11-13 22:07:47 +0200

    DB

--- lib/view_model/database/local/shared_prefrences/string/failures.dart
+++
@@ -1,3 +0,0 @@
-const String SERVER_FAILURE_MESSAGE = 'Please try again later .';
-const String EMPTY_CACHE_FAILURE_MESSAGE = 'No Data';
-const String OFFLINE_FAILURE_MESSAGE = 'Please Check your Internet Connection';
--- lib/view_model/database/local/shared_prefrences/string/messages.dart
+++
@@ -1,3 +0,0 @@
-const ADD_SUCCESS_MESSAGE = "Post Added Successfully";
-const DELETE_SUCCESS_MESSAGE = "Post Deleted Successfully";
-const UPDATE_SUCCESS_MESSAGE = "Post Updated Successfully";
